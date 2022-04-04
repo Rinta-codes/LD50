913948544dc1cefbd5ead4e72ceb303a270f6036
Rinta <55794209+Rinta-codes@users.noreply.github.com>
2022-04-04 09:27:50 +0100

    Adding labels to Main Menu

--- LD50/Scenes/Menus/MainMenu.cs
+++ LD50/Scenes/Menus/MainMenu.cs
@@ -11,10 +11,15 @@ namespace LD50.Scenes.Menus
     {
         public MainMenu() : base(Vector2.Zero)
         {
-            var startGameButton = new Button(new Vector4(.8f, .8f, .8f, 1), new Vector4(.5f, .5f, .5f, 1), new Vector2(Globals.windowSize.X / 2, 500), new Vector2(400, 200), 5, Graphics.DrawLayer.UI, true);
+            var tutorialLine1 = new Label($"The dragon is on a rampage!", TextAlignment.CENTER, Globals.genericLabelTextColour, new Vector2(Globals.ScreenResolutionX / 2, 120), Globals.genericLabelFontSize, true);
+
+            var startGameButton = new Button(Globals.buttonFillColour, Globals.buttonBorderColour, new Vector2(Globals.windowSize.X / 2, 500), new Vector2(400, 200), 5, Graphics.DrawLayer.UI, true);
             startGameButton.SetText("Play", TextAlignment.CENTER, new Vector4(0, 0, 0, 1));
             startGameButton.OnClickAction = () => { Globals.currentScene = (int)Scenes.DRIVING; Globals.hud.IsHidden = false; BackgroundMusicManager.PlayMusic("Audio/Music/Ld50Rustig.wav"); };
+            
+            
             uiElements.Add(startGameButton);
+            
             Globals.hud.IsHidden = true;
         }
     }
