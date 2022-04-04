75b58b354891f5cb63bc024c357c95c4ffcd2b5f
ilan hartskeerl <ilan@hartskeerl.nl>
2022-04-05 00:33:49 +0200

    Dragon hard now

--- LD50/Logic/Enemies/Dragon.cs
+++ LD50/Logic/Enemies/Dragon.cs
@@ -22,6 +22,8 @@ namespace LD50.Logic.Enemies
 
         private float _formChangeTimer;
 
+        private Weapon _weapon2 = null;
+
         public Dragon() : base(TexName.DRAGON, new Vector2(640, 640), Balance.DragonMaxHP, new DragonWeapon())
         {
             ChangeForm();
@@ -35,30 +37,40 @@ namespace LD50.Logic.Enemies
                 case 0:
                     _sprite.SetColour(possibleDragonColours[0]);
                     _weapon = new BaseGun();
+                    _weapon2 = new BaseGun();
                     _weapon.ProjectileRange = 3000;
+                    _weapon2.ProjectileRange = 3000;
                     break;
                 case 1:
                     _sprite.SetColour(possibleDragonColours[1]);
                     _weapon = new BetterGun();
+                    _weapon2 = new BetterGun();
                     _weapon.ProjectileRange = 3000;
+                    _weapon2.ProjectileRange = 3000;
                     break;
                 case 2:
                     _sprite.SetColour(possibleDragonColours[2]);
                     _weapon = new DragonWeapon();
+                    _weapon2 = new DragonWeapon();
                     break;
                 case 3:
                     _sprite.SetColour(possibleDragonColours[3]);
                     _weapon = new Sniper();
+                    _weapon2 = new Sniper();
                     break;
                 case 4:
                     _sprite.SetColour(possibleDragonColours[4]);
                     _weapon = new FastGun();
+                    _weapon2 = new FastGun();
                     _weapon.ProjectileRange = 3000;
+                    _weapon2.ProjectileRange = 3000;
                     break;
                 case 5:
                     _sprite.SetColour(possibleDragonColours[5]);
                     _weapon = new RocketLauncher();
+                    _weapon2 = new RocketLauncher();
                     _weapon.ProjectileRange = 3000;
+                    _weapon2.ProjectileRange = 3000;
                     break;
             }
         }
@@ -73,12 +85,16 @@ namespace LD50.Logic.Enemies
             if (potentialTargets.Count <= 0) return true;
             _target = potentialTargets[Globals.rng.Next(potentialTargets.Count)];
 
+            _weapon2.Update();
+
             _weapon.Attack(this, _target.Position - Position, Position);
+            _weapon2.Attack(this, _target.Position - Position + new Vector2(Globals.rng.Next(-250, 250), Globals.rng.Next(-250, 250)), Position);
 
             if (_formChangeTimer < 0)
             {
                 ChangeForm();
                 _weapon.ResetCooldown();
+                _weapon2.ResetCooldown();
             }
 
 
